e5b0fad999dff541c7685878faa61bf8234491ef
rupert-ong <rupert.ong@gmail.com>
2021-04-21 21:56:29 -0400

    chore: add dist to gitignore

--- packages/base-text/dist/base-text.esm.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-var Text = function Text(_ref) {
-  var children = _ref.children;
-  return /*#__PURE__*/React.createElement("p", null, children);
-};
-
-export { Text };
