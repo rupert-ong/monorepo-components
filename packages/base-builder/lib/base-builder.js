efaa6db6982942b40142dbd7073a1edaa1c65d2f
rupert-ong <rupert.ong@gmail.com>
2021-04-28 09:15:27 -0400

    build: add TypeScript compiler to builder

--- packages/base-builder/lib/base-builder.js
+++ packages/base-builder/lib/base-builder.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 const rollup = require("rollup");
 const path = require("path");
+const peerDepsExternal = require("rollup-plugin-peer-deps-external");
+const commonjs = require("@rollup/plugin-commonjs");
 const resolve = require("@rollup/plugin-node-resolve").default;
 const babel = require("@rollup/plugin-babel").default;
 const postcss = require("rollup-plugin-postcss");
+const typescript = require("rollup-plugin-typescript2");
+const del = require("rollup-plugin-delete");
 
 const currentWorkingPath = process.cwd();
-const { src, name } = require(path.join(currentWorkingPath, "package.json"));
+const {
+  src,
+  name,
+  devDependencies = {},
+  peerDependencies = {},
+} = require(path.join(currentWorkingPath, "package.json"));
 
 const inputPath = path.join(currentWorkingPath, src);
 
@@ -14,16 +23,55 @@ const fileName = name.replace("@rupertong/", "");
 
 const inputOptions = {
   input: inputPath,
-  external: ["react"],
+  external: [...Object.keys(devDependencies), ...Object.keys(peerDependencies)],
   plugins: [
-    resolve(),
+    peerDepsExternal(),
+    resolve({
+      extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx", ".json", ".node"],
+    }),
+    commonjs(),
+    typescript({
+      tsconfig: "../../tsconfig.json",
+      rollupCommonJSResolveHack: true,
+      useTsconfigDeclarationDir: true,
+      // clean: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          baseUrl: currentWorkingPath,
+          declaration: true,
+          declarationMap: true,
+          declarationDir: path.join(currentWorkingPath, "dist", "types"),
+        },
+        include: ["./lib/**.*.+(ts|tsx)"],
+        exclude: [
+          "node_modules",
+          "dist",
+          "/**/*.stor+(y|ies).*",
+          "/**/*.+(spec|test).*",
+        ],
+      },
+    }),
     babel({
-      presets: ["@babel/preset-env", "@babel/preset-react"],
+      extensions: [".js", ".ts", ".tsx", ".jsx"],
+      exclude: [/core-js/],
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            corejs: 3,
+            modules: false,
+            targets: "> 0.25%, ie 11, not op_mini all",
+            useBuiltIns: "usage",
+          },
+        ],
+        "@babel/preset-react",
+      ],
       babelHelpers: "bundled",
     }),
     postcss({
       modules: true,
     }),
+    del({ targets: [`${path.join(currentWorkingPath, "dist")}`] }),
   ],
 };
 
@@ -31,10 +79,12 @@ const outputOptions = [
   {
     file: `dist/${fileName}.cjs.js`,
     format: "cjs",
+    sourcemap: true,
   },
   {
     file: `dist/${fileName}.esm.js`,
     format: "esm",
+    sourcemap: true,
   },
 ];
 
