6418794d80df6ec2d874f99b0ca62b5e12cef1df
rupert-ong <rupert.ong@gmail.com>
2021-05-03 09:47:28 -0400

    test(base-ui): add Foo and Bar tests

---
+++ packages/base-ui/lib/components/Bar/Bar.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Bar from '.';
+
+describe('<Bar />', () => {
+  it('renders correctly', () => {
+    render(<Bar id="test" data-testid="test-id" />);
+    expect(screen.getByText(/bar/i)).toBeInTheDocument();
+  });
+});
