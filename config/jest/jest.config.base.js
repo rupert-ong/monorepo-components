908b1e0c8a99c83ae596318ce9dd239169d964ca
rupert-ong <rupert.ong@gmail.com>
2021-05-03 13:20:23 -0400

    chore(core): add jest mocks and refactor build scripts

--- config/jest/jest.config.base.js
+++ config/jest/jest.config.base.js
@@ -5,6 +5,7 @@ module.exports = {
   moduleDirectories: ['node_modules'],
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/config/jest/__mocks__/fileMock.ts',
     '@rupertong/(.+)': '<rootDir>/packages/$1/lib',
   },
 };
