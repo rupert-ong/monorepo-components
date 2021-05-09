54a68e89d5c00016f6a52699f7a9fb16e967fe16
rupert-ong <rupert.ong@gmail.com>
2021-05-08 21:01:51 -0400

    feat: add prerelease demo package

---
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+
+/**
+ * Pre-release demo
+ */
+export const Component = (): ReactElement => {
+  return <div>Component (Pre-release)</div>;
+};
+
+export default Component;
