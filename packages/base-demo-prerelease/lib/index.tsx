86afb19363722d7891ddba9d870ad1f0b73b2da0
rupert-ong <rupert.ong@gmail.com>
2021-05-08 21:43:45 -0400

    feat: add theta pre-release test

--- packages/base-demo-prerelease/lib/index.tsx
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
  * Pre-release demo
  */
 export const Component = (): ReactElement => {
-  return <div>Component (Pre-release: test preid to beta)</div>;
+  return <div>Component (Pre-release: test preid to theta)</div>;
 };
 
 export default Component;
