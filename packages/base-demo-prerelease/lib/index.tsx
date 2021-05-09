175979030723d5842e0db7685f8833d058caedf8
rupert-ong <rupert.ong@gmail.com>
2021-05-09 14:37:46 -0400

    feat: add alpha feature test

--- packages/base-demo-prerelease/lib/index.tsx
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
  * Pre-release demo
  */
 export const Component = (): ReactElement => {
-  return <div>Component (Graduate attempt)</div>;
+  return <div>Component (0.2.0 alpha attempt)</div>;
 };
 
 export default Component;
