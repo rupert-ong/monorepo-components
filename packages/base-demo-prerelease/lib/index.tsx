03899451bf376aee678d0eab62fed804a3ce1220
rupert-ong <rupert.ong@gmail.com>
2021-05-08 21:28:53 -0400

    feat: add beta test feature

--- packages/base-demo-prerelease/lib/index.tsx
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
  * Pre-release demo
  */
 export const Component = (): ReactElement => {
-  return <div>Component (Pre-release: test feature)</div>;
+  return <div>Component (Pre-release: test preid to beta)</div>;
 };
 
 export default Component;
