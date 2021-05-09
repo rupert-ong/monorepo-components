feb5d09ed37f1815de27a02e716673f2dc90039e
rupert-ong <rupert.ong@gmail.com>
2021-05-08 21:00:39 -0400

    feat: add monorepo package template

---
+++ src/templates/template-package/lib/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+
+/**
+ * Component summary for Storybook documentation
+ */
+export const Component = (): ReactElement => {
+  return <div>Component</div>;
+};
+
+export default Component;
