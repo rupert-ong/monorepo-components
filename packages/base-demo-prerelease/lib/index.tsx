804cae36edd7ab5f7c3becc360655c7069c8974b
rupert-ong <rupert.ong@gmail.com>
2021-05-09 21:24:08 -0400

    feat: add final feature for release

--- packages/base-demo-prerelease/lib/index.tsx
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
  * Pre-release demo
  */
 export const Component = (): ReactElement => {
-  return <div>Component (0.2.0 beta fix #2)</div>;
+  return <div>Component</div>;
 };
 
 export default Component;
