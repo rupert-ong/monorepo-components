0e1203899a01e633f6ab027e9712bbe4e6833dbb
rupert-ong <rupert.ong@gmail.com>
2021-05-09 21:09:16 -0400

    feat: add alpha pre-release feature

--- packages/base-demo-prerelease/lib/index.tsx
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
  * Pre-release demo
  */
 export const Component = (): ReactElement => {
-  return <div>Component (0.2.0 alpha fix)</div>;
+  return <div>Component (0.2.0 alpha feature)</div>;
 };
 
 export default Component;
