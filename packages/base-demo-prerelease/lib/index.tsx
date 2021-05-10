715b73cd0616c962d54fa0d103aa0f7e9e2a073b
rupert-ong <rupert.ong@gmail.com>
2021-05-09 21:33:24 -0400

    feat: add alpha feature

--- packages/base-demo-prerelease/lib/index.tsx
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
  * Pre-release demo
  */
 export const Component = (): ReactElement => {
-  return <div>Component Feature #1</div>;
+  return <div>Component alpha 0.3.0 #0</div>;
 };
 
 export default Component;
