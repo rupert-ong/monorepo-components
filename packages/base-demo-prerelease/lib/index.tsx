6eb743d35308c74506f7abe10f1447261b812747
rupert-ong <rupert.ong@gmail.com>
2021-05-09 21:14:42 -0400

    fix: add beta fix

--- packages/base-demo-prerelease/lib/index.tsx
+++ packages/base-demo-prerelease/lib/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
  * Pre-release demo
  */
 export const Component = (): ReactElement => {
-  return <div>Component (0.2.0 alpha feature #2)</div>;
+  return <div>Component (0.2.0 beta fix #1)</div>;
 };
 
 export default Component;
